cfbbe3be2bb71c35c93270578f3009c9791a50cf
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-11-05 13:42:11 +1100

    posting and updating are working. Page reload isn't great.

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -12,19 +12,26 @@ module.exports = function(app) {
   app.post("/api/burgers", function(req, res) {
       db.Burger.create({
         burger_name: req.body.burger_name
-      }).then(function(results) {
-        var burgers = {burgers: results}
-        res.render("index", burgers)
+      }).then(function(result) {
+        db.Burger.findAll({}).then(function(results) {
+          var burgers = {burgers: results}
+          res.render("index", burgers)
+        });
       });
   });
 
   app.put("/api/burgers/:id", function(req, res) {
-      db.Burger.update({
-        burger_name: req.body.burger_name,
-        devoured: req.body.devoured
-      }).then(function(results) {
-        var burgers = {burgers: results}
-        res.render("index", burgers)
+      db.Burger.update(
+        {devoured: true},
+        {where: {
+          id: req.params.id
+          }
+        }
+      ).then(function(result) {
+        db.Burger.findAll({}).then(function(results) {
+          var burgers = {burgers: results}
+          res.render("index", burgers)
+        });
       });
   });
 
